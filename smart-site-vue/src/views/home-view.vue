653c19da59f4ffc771c3c2015ebd989d95945a12
ALiang02 <1745346447@qq.com>
2020-12-17 15:45:51 +0800

    路由bug修改

--- smart-site-vue/src/views/home-view.vue
+++ smart-site-vue/src/views/home-view.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <router-link to="contract">合同管理</router-link> |
-    <router-link to="project">项目管理</router-link> |
-    <router-link to="employee">人员管理</router-link> |
-    <router-link to="group">小组管理</router-link> |
-    <router-link to="client">委托方管理</router-link>
+    <router-link to="/home/contract">合同管理</router-link> |
+    <router-link to="/home/project">项目管理</router-link> |
+    <router-link to="/home/employee">人员管理</router-link> |
+    <router-link to="/home/group">小组管理</router-link> |
+    <router-link to="/home/client">委托方管理</router-link>
     <router-view></router-view>
   </div>
 </template>
